ca3bb94a2c75636c1563f1560200e5c458703b02
Ayush <ayushsakhuja@gmail.com>
2015-01-07 20:27:08 +0530

    Commit Sequel

--- app/src/main/java/com/sakhuja/ayush/secretsafe/Splash.java
+++ app/src/main/java/com/sakhuja/ayush/secretsafe/Splash.java
@@ -36,6 +36,7 @@ public class Splash extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_splash, menu);
         return true;
+        //LOL
     }
 
     @Override
---
+++ app/src/main/java/com/sakhuja/ayush/secretsafe/a.java
@@ -0,0 +1,7 @@
+package com.sakhuja.ayush.secretsafe;
+
+/**
+ * Created by Ayush on 1/7/2015.
+ */
+public class a {
+}
